1734c9e07b4fbdcd662c57f90f2bdc7d6f5a3067
Fernando Saraiva <fpsaraiva@gmail.com>
2022-01-06 17:30:10 -0300

    adding signup validation

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.use('/auth', authRoutes);
 app.use((error, req, res, next) => {
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: message })
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 })
 
 mongoose.connect(
