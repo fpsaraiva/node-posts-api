155b206323ee91df35ac023ed30b851d385319ab
Fernando Saraiva <fpsaraiva@gmail.com>
2022-01-07 18:44:13 -0300

    clear post-user relations

--- controllers/feed.js
+++ controllers/feed.js
@@ -154,30 +154,37 @@ exports.updatePost = (req, res, next) => {
 exports.deletePost = (req, res, next) => {
   const postId = req.params.postId;
   Post.findById(postId)
-      .then(post => {
-        if(!post) {
-          const error = new Error('Could not find post.');
-          error.statusCode = 404;
-          throw error;
-        }
-        if(post.creator.toString() !== req.userId) {
-          const error = new Error('Not authorized.');
-          error.statusCode = 403;
-          throw error;
-        }
-        clearImage(post.imageUrl);
-        return Post.findByIdAndRemove(postId);
-      })
-      .then(result => {
-        res.status(200).json({ message: 'Deleted post' });
-      })
-      .catch(err => {
-        if(!err.statusCode) {
-          err.statusCode = 500;
-        }
-        next(err);
-      });
-}
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(result => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Deleted post.' });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
 const clearImage = filePath => {
   filePath = path.join(__dirname, '..', filePath);
